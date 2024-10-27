af3169fefd21662370e8095686075c03bad76f15
Alejandro Vazquez <alexx.vazquezz@gmail.com>
2024-10-27 09:59:23 -0300

    changed headless to true

--- tests/support/hooks.js
+++ tests/support/hooks.js
@@ -51,7 +51,7 @@ BeforeAll(async function() {
 Before(async function() {
   // Lauches browser for each new test
   browser = await chromium.launch({ 
-    headless: false, 
+    headless: true, 
   }); 
 
   context = await browser.newContext();
